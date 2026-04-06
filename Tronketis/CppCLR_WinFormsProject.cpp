42ec90418792e5f7940b429c500e3195b5fe99e3
shilamaki <shila.makiabadi@estudiantat.upc.edu>
2026-04-06 12:17:42 +0200

    Estructura base + inicio login

--- Tronketis/CppCLR_WinFormsProject.cpp
+++ Tronketis/CppCLR_WinFormsProject.cpp
@@ -7,7 +7,7 @@ using namespace System;
 //    return 0;
 // }
 
-#include "Presentacio/IniciarSesionForm.h"
+#include "IniciarSesionForm.h"
 
 using namespace System::Windows::Forms;
 
@@ -16,6 +16,6 @@ int main()
 {
   Application::EnableVisualStyles();
   Application::SetCompatibleTextRenderingDefault(false);
-  Application::Run(gcnew CppCLRWinFormsProject::Form1());
+  Application::Run(gcnew Tronketis::IniciarSesionForm());
   return 0;
 }
