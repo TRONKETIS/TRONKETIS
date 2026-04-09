9deb08dbe82adfadf06e47fd7a176cad2d4ddcc9
sergio1313 <sergio.conde.cabezudo@estudiantat.upc.edu>
2026-04-09 11:41:21 +0200

    Actualizacion general de todos los archivos editados

--- Tronketis/Form1.cpp
+++ Tronketis/Form1.cpp
@@ -1,3 +1,23 @@
 #include "pch.h"
-// #include "Form1.h"
+#include "Form1.h"
+#include "CercadorUsuari.h"
+
+System::Void Form1::btnBuscar_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    String^ username = txtUsername->Text;
+
+    String^ id;
+    String^ password;
+    String^ rol;
+
+    if (CercadorUsuari::CercaPerUsername(username, id, password))
+    {
+        lblResultado->Text = "ID: " + id + " | Password: " + password+ " | Rol: " + rol;
+    }
+    else
+    {
+        lblResultado->Text = "Usuario no encontrado";
+    }
+    
+}
 
