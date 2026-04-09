5f585f099850896231753e534cf4516e95c66c57
sergio1313 <sergio.conde.cabezudo@estudiantat.upc.edu>
2026-04-09 11:51:44 +0200

    inhabilitar colla form1

--- Tronketis/Form1.cpp
+++ Tronketis/Form1.cpp
@@ -1,3 +1,17 @@
 #include "pch.h"
-// #include "Form1.h"
+#include "Form1.h"
+#include "CercadorColla.h"
 
+private: System::Void btnInhabilitar_Click(System::Object^ sender, System::EventArgs^ e) {
+
+    int idColla = Convert::ToInt32(txtIdColla->Text);
+
+    bool ok = CercadorColla::InhabilitarColla(idColla);
+
+    if (ok) {
+        lblResultado->Text = "Colla inhabilitada correctamente";
+    } else {
+        lblResultado->Text = "Error al inhabilitar la colla";
+    }
+    
+}
