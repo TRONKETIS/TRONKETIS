7928d2e122453daed88d98fcc98fba83e9c624d3
sergio1313 <sergio.conde.cabezudo@estudiantat.upc.edu>
2026-05-05 18:58:32 +0200

    Inhabilitar colla per nom connectat a base de dades

--- Tronketis/Form1.cpp
+++ Tronketis/Form1.cpp
@@ -1,17 +1,2 @@
 #include "pch.h"
 #include "Form1.h"
-#include "CercadorColla.h"
-
-private: System::Void btnInhabilitar_Click(System::Object^ sender, System::EventArgs^ e) {
-
-    int idColla = Convert::ToInt32(txtIdColla->Text);
-
-    bool ok = CercadorColla::InhabilitarColla(idColla);
-
-    if (ok) {
-        lblResultado->Text = "Colla inhabilitada correctamente";
-    } else {
-        lblResultado->Text = "Error al inhabilitar la colla";
-    }
-    
-}
--- Tronketis/Form1.h
+++ Tronketis/Form1.h
@@ -1,59 +1,104 @@
-#pragma once
+﻿#pragma once
+#include "CercadorColla.h"
 
 namespace CppCLRWinFormsProject {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+    using namespace System;
+    using namespace System::Windows::Forms;
 
-	/// <summary>
-	/// Summary for Form1
-	/// </summary>
-	public ref class Form1 : public System::Windows::Forms::Form
-	{
-	public:
-		Form1(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+    public ref class Form1 : public Form
+    {
+    public:
+        Form1(void) { InitializeComponent(); }
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~Form1()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+    protected:
+        ~Form1() { if (components) delete components; }
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+    private:
+        System::Windows::Forms::TextBox^ txtNomColla;
+        System::Windows::Forms::Button^ btnInhabilitar;
+        System::Windows::Forms::Label^ lblResultado;
+        System::Windows::Forms::Label^ label1;
+        System::ComponentModel::Container^ components;
 
-#pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-		}
-#pragma endregion
-	};
+        void InitializeComponent(void)
+        {
+            this->txtNomColla = (gcnew System::Windows::Forms::TextBox());
+            this->btnInhabilitar = (gcnew System::Windows::Forms::Button());
+            this->lblResultado = (gcnew System::Windows::Forms::Label());
+            this->label1 = (gcnew System::Windows::Forms::Label());
+            this->SuspendLayout();
+
+            // FONDO
+            this->BackColor = System::Drawing::Color::FromArgb(245, 245, 250);
+
+            // LABEL
+            this->label1->AutoSize = true;
+            this->label1->Location = System::Drawing::Point(50, 40);
+            this->label1->Text = L"Nom de la colla:";
+            this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Bold));
+            this->label1->ForeColor = System::Drawing::Color::FromArgb(60, 60, 60);
+            this->label1->TabIndex = 0;
+
+            // TEXTBOX
+            this->txtNomColla->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
+            this->txtNomColla->Location = System::Drawing::Point(50, 65);
+            this->txtNomColla->Size = System::Drawing::Size(280, 34);
+            this->txtNomColla->TabIndex = 1;
+
+            // BOTÓN
+            this->btnInhabilitar->Location = System::Drawing::Point(50, 115);
+            this->btnInhabilitar->Size = System::Drawing::Size(150, 38);
+            this->btnInhabilitar->Text = L"🚫 Inhabilitar";
+            this->btnInhabilitar->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
+            this->btnInhabilitar->BackColor = System::Drawing::Color::FromArgb(200, 50, 50);
+            this->btnInhabilitar->ForeColor = System::Drawing::Color::White;
+            this->btnInhabilitar->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+            this->btnInhabilitar->FlatAppearance->BorderSize = 0;
+            this->btnInhabilitar->Cursor = System::Windows::Forms::Cursors::Hand;
+            this->btnInhabilitar->TabIndex = 2;
+            this->btnInhabilitar->Click += gcnew System::EventHandler(this, &Form1::btnInhabilitar_Click);
+
+            // RESULTADO
+            this->lblResultado->Location = System::Drawing::Point(50, 175);
+            this->lblResultado->Size = System::Drawing::Size(500, 30);
+            this->lblResultado->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
+            this->lblResultado->TabIndex = 3;
+
+            // FORM
+            this->ClientSize = System::Drawing::Size(600, 280);
+            this->Controls->Add(this->label1);
+            this->Controls->Add(this->txtNomColla);
+            this->Controls->Add(this->btnInhabilitar);
+            this->Controls->Add(this->lblResultado);
+            this->Name = L"Form1";
+            this->Text = L"Inhabilitar Colla";
+            this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+            this->ResumeLayout(false);
+            this->PerformLayout();
+        }
+
+    private:
+        System::Void btnInhabilitar_Click(System::Object^ sender, System::EventArgs^ e)
+        {
+            String^ nom = txtNomColla->Text->Trim();
+
+            if (nom == "")
+            {
+                lblResultado->Text = "Introdueix el nom de la colla.";
+                return;
+            }
+
+            if (CercadorColla::InhabilitarColla(nom))
+            {
+                lblResultado->ForeColor = System::Drawing::Color::FromArgb(0, 150, 0);
+                lblResultado->Text = "Colla '" + nom + "' inhabilitada correctament.";
+            }
+            else
+            {
+                lblResultado->ForeColor = System::Drawing::Color::FromArgb(200, 50, 50);
+                lblResultado->Text = "No s'ha trobat cap colla amb aquest nom.";
+            }
+        }
+    };
 }
